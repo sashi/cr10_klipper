d8298ac29dafa23b19476daf3502f1ac66e6d5d9
Sashi <iam@sashi.ca>
2021-05-02 17:45:46 -0700

    first commit

---
+++ fluidd/precache-manifest.a91585d72a02fc6d8d66b20922ce8ad2.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154ccd182313c345d3b9dd8571931eb5",
+    "url": "/6e3d5ec6699a8d147d29486d96b7d746.wasm"
+  },
+  {
+    "revision": "ac28013aaee6ba96e4dfd666063d0692",
+    "url": "/config.json"
+  },
+  {
+    "revision": "c55b1e490ef7367e2c83",
+    "url": "/css/app.d6bc3738.css"
+  },
+  {
+    "revision": "78d3c61be69c53db8686",
+    "url": "/css/chunk-vendors.4c962d95.css"
+  },
+  {
+    "revision": "3bb20e2e2531f1d718ff39721bb93034",
+    "url": "/editor.theme.json"
+  },
+  {
+    "revision": "8214fdff0358fc2f764851b86f8a3099",
+    "url": "/editor.worker.js"
+  },
+  {
+    "revision": "b13daaad214ef227a36fefd95d924380",
+    "url": "/fonts/codicon.b13daaad.ttf"
+  },
+  {
+    "revision": "75bbfb53c29f127f159c9e87069a6d3b",
+    "url": "/fonts/raleway-all-400-normal.75bbfb53.woff"
+  },
+  {
+    "revision": "8bf4ee756fdcafb4c17511a49d2f49db",
+    "url": "/fonts/raleway-cyrillic-400-normal.8bf4ee75.woff2"
+  },
+  {
+    "revision": "61d8e28767a598cc365a6b58358b5de1",
+    "url": "/fonts/raleway-cyrillic-ext-400-normal.61d8e287.woff2"
+  },
+  {
+    "revision": "7bc6cb4378557439b87b4460024fb904",
+    "url": "/fonts/raleway-latin-400-normal.7bc6cb43.woff2"
+  },
+  {
+    "revision": "8c8f3a7af9f61a0f6c77e4bd4c3866ad",
+    "url": "/fonts/raleway-latin-ext-400-normal.8c8f3a7a.woff2"
+  },
+  {
+    "revision": "59568860824fecc9ecb1e06df16b395c",
+    "url": "/fonts/raleway-vietnamese-400-normal.59568860.woff2"
+  },
+  {
+    "revision": "c46434d46a72919eb141228dd0144b6c",
+    "url": "/fonts/roboto-all-300-normal.c46434d4.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/fonts/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "851a2b5a8394eb1b868678bfd31a1a8a",
+    "url": "/fonts/roboto-all-500-normal.851a2b5a.woff"
+  },
+  {
+    "revision": "cf654de4c5d988526828f731f299d30a",
+    "url": "/fonts/roboto-all-700-normal.cf654de4.woff"
+  },
+  {
+    "revision": "a5383450b25d7490ad1a6e15bd0b176d",
+    "url": "/fonts/roboto-cyrillic-300-normal.a5383450.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/fonts/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "7fd643e63fc847f47521a2604072ce9c",
+    "url": "/fonts/roboto-cyrillic-500-normal.7fd643e6.woff2"
+  },
+  {
+    "revision": "9d484aa99b936dfe89b6b12df530d138",
+    "url": "/fonts/roboto-cyrillic-700-normal.9d484aa9.woff2"
+  },
+  {
+    "revision": "d69a2de852e8018e514374e6856b1daa",
+    "url": "/fonts/roboto-cyrillic-ext-300-normal.d69a2de8.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/fonts/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "378698af3b6776fd1e3ee0a51c3d7b87",
+    "url": "/fonts/roboto-cyrillic-ext-500-normal.378698af.woff2"
+  },
+  {
+    "revision": "2522a38e3389f585569d5f19a7cab696",
+    "url": "/fonts/roboto-cyrillic-ext-700-normal.2522a38e.woff2"
+  },
+  {
+    "revision": "f7059272fd8e0226350501393f4450c0",
+    "url": "/fonts/roboto-greek-300-normal.f7059272.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/fonts/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "665639f6a0519416ad2904f1c218ea30",
+    "url": "/fonts/roboto-greek-500-normal.665639f6.woff2"
+  },
+  {
+    "revision": "16d9701c9975c56671c57bd242ccb1be",
+    "url": "/fonts/roboto-greek-700-normal.16d9701c.woff2"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/fonts/roboto-latin-300-normal.ef7c6637.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/fonts/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/fonts/roboto-latin-500-normal.020c97dc.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/fonts/roboto-latin-700-normal.2735a3a6.woff2"
+  },
+  {
+    "revision": "e83b8f976d12362b33c7047c09ef586e",
+    "url": "/fonts/roboto-latin-ext-300-normal.e83b8f97.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/fonts/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "b1b808436b5eafe9a683fff584d80fea",
+    "url": "/fonts/roboto-latin-ext-500-normal.b1b80843.woff2"
+  },
+  {
+    "revision": "188b397610a9a71bd931a3bd04ee2197",
+    "url": "/fonts/roboto-latin-ext-700-normal.188b3976.woff2"
+  },
+  {
+    "revision": "484cddf4a27f89deb619b0c5c38bdcfd",
+    "url": "/fonts/roboto-vietnamese-300-normal.484cddf4.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/fonts/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  },
+  {
+    "revision": "16423fb4da5bb6d54592839d6cd8e1be",
+    "url": "/fonts/roboto-vietnamese-500-normal.16423fb4.woff2"
+  },
+  {
+    "revision": "d9600d97b65b16ddb8527136d38f8160",
+    "url": "/fonts/roboto-vietnamese-700-normal.d9600d97.woff2"
+  },
+  {
+    "revision": "ad073d7e08223a83b933b5976b0e6224",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c55b1e490ef7367e2c83",
+    "url": "/js/app.8f25b43d.js"
+  },
+  {
+    "revision": "8f8544c7038f15bf7412",
+    "url": "/js/chunk-2d0c512b.dfb41034.js"
+  },
+  {
+    "revision": "78d3c61be69c53db8686",
+    "url": "/js/chunk-vendors.7d5bd44a.js"
+  },
+  {
+    "revision": "33aefd155857d97bb850",
+    "url": "/js/grammar-gcode-tmLanguage-json.c29ab31a.js"
+  },
+  {
+    "revision": "0a99d8f1d22af8778f27",
+    "url": "/js/grammar-klipper-config-tmLanguage-json.92f50f36.js"
+  },
+  {
+    "revision": "c3da4ceac572e23e4846",
+    "url": "/js/locale-cn-yaml.911709ff.js"
+  },
+  {
+    "revision": "7186e65b880b14cb3b75",
+    "url": "/js/locale-de-yaml.ff376c11.js"
+  },
+  {
+    "revision": "dcc462b41809e6260ee5",
+    "url": "/js/locale-en-yaml.50534ce6.js"
+  },
+  {
+    "revision": "019eec6706536cd6a771",
+    "url": "/js/locale-fr-yaml.fd09cc21.js"
+  },
+  {
+    "revision": "ce56f141fa0e8673caa5",
+    "url": "/js/locale-hu-yaml.642c39db.js"
+  },
+  {
+    "revision": "95e3993a6b5f822c2d70",
+    "url": "/js/locale-ko-yaml.25d32c14.js"
+  },
+  {
+    "revision": "77c81c7d426a9780cdd3",
+    "url": "/js/locale-nl-yaml.0055c90e.js"
+  },
+  {
+    "revision": "6026d2fe32228c781e9e",
+    "url": "/js/locale-pt-yaml.a1ab53a7.js"
+  },
+  {
+    "revision": "1b37bf3787dc26cd2376",
+    "url": "/js/locale-ru-yaml.f4f0e3f7.js"
+  },
+  {
+    "revision": "7fce6679c8ceae507d595bfe8aff1dc3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
